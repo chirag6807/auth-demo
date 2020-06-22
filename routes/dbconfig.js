554f061af34d32c622cecd2bd91c33f50732ce32
chirag <radix@PC0413>
2020-06-22 10:35:10 +0530

    first commit

---
+++ routes/dbconfig.js
@@ -0,0 +1,11 @@
+exports.config = {
+    user: 'sa',
+    password: 'sa123',
+    server: 'PC0413',
+    database: 'Covid19DB',
+    parseJSON: true,
+    options: {
+        enableArithAbort: true,
+        encrypt: true
+    },
+}
